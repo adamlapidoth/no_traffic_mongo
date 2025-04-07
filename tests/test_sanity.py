0b448038f67141eab92633b97a67e75ce358341a
Adam Lapidoth <adamlapidoth@gmail.com>
2025-04-07 12:50:06 +0300

    code cleanup + create sanity tests

---
+++ tests/test_sanity.py
@@ -0,0 +1,8 @@
+def test_mouse(mouse):
+    assert mouse.name == "mouse"
+    assert mouse.price == 50
+
+
+def test_laptop(laptop):
+    assert laptop.name == "laptop"
+    assert laptop.price == 1200
